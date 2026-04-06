134ecc7b754433168c3aebd4365f900fea6ae170
NatiqueAsghar <mdnatiqueasghar@gmail.com>
2026-04-07 00:52:41 +0530

    added base url

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,7 +9,7 @@ import { UserContextProvider } from "./util/UserContext.jsx";
 if (import.meta.env.DEV) {
   axios.defaults.baseURL = import.meta.env.VITE_LOCALHOST;
 } else {
-  axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL;
+  axios.defaults.baseURL = "/api";
 }
 axios.defaults.withCredentials = true;
 
