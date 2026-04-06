802bf44f3abad0a75946e34909e8fbecc8696573
NatiqueAsghar <mdnatiqueasghar@gmail.com>
2026-04-06 23:25:55 +0530

    Add health to keep alive

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,6 +12,11 @@ app.use(
   })
 );
 
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
